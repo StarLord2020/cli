9630ac92b082732ec9d3bf9b6be1c0af9d77ce6f
Ivan <54555014+StarLord2020@users.noreply.github.com>
2020-02-20 23:53:23 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import Vuex from 'vuex';
+import VueRouter from 'vue-router'
+import router from './routes'
+import store from './Store/index'
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+Vue.use(VueFire)
+firebase.initializeApp({
+  apiKey: "AIzaSyBiCAV7HLZMGjSHqMn-S_o1f7lrmixGvCQ",
+  authDomain: "mypr-17866.firebaseapp.com",
+  databaseURL: "https://mypr-17866.firebaseio.com",
+  projectId: "mypr-17866",
+  storageBucket: "mypr-17866.appspot.com",
+  messagingSenderId: "193365158587",
+  appId: "1:193365158587:web:134c53d61f83c7552dd79e",
+  measurementId: "G-7EB0YSDRG7"
+})
+export const db = firebase.firestore()
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,router
 }).$mount('#app')
