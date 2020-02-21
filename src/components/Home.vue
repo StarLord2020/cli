da0982da71c43d5c6ee4e69dd0032e095a030e07
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-21 11:02:09 +0300

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,27 +7,17 @@
               <div class="flex  ">
                   <div class="block all:hidden">
                       <button @click="toggle" class=" red flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-                          <svg  class="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>     
+                          <svg  class="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg> 
                       </button>
-                        <button @click="breakUser()">Выйти</button>
                   </div>
                   <div class="flex items-center flex-no-shrink text-white ml-6">
                       <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
                       <span class="font-semibold text-xl tracking-tight">Keep Finance</span>
-                  </div>
+                  </div>    
                   <nav class=" menu-list flex  bg-teal  bg-green-400 absolute top-20">
                       <div :class="open ? 'block': 'hidden'" class="w-full flex-grow all:flex p-6 all:items-center all:w-auto">
                           <div class="text-all all:flex-grow">
-                              <!-- <a href="#responsive-header" class="no-underline block mt-4 all:inline-block all:mt-0 text-teal-lighter hover:text-white mr-4">
-                              Docs
-                              </a> -->
-                              <router-link class="no-underline block mt-4 all:inline-block all:mt-0 text-teal-lighter hover:text-white mr-4" exact active-class="font-bold" to="/">Home</router-link>
-                              <a href="#responsive-header" class="no-underline block mt-4 all:inline-block all:mt-0 text-teal-lighter hover:text-white mr-4">
-                              &#xed91;
-                              </a>
-                              <a href="#responsive-header" class="no-underline block mt-4 all:inline-block all:mt-0 text-teal-lighter hover:text-white">
-                              Blog
-                              </a>
+                              <div class="link" @click="breakUser()">Выйти</div>
                           </div>             
                       </div>
                       <div>
@@ -115,5 +105,11 @@ import List from './List'
   }
   .container-mobile {
       padding:0 15px;
+  }
+  .link:hover {
+    color:#fff;
+  }
+  .link{
+    cursor: pointer;
   }
 </style>
