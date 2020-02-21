b226c3d655031aef440c643348c24facf1fb2310
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-21 10:28:14 +0300

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,6 +9,7 @@
                       <button @click="toggle" class=" red flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
                           <svg  class="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>     
                       </button>
+                        <button @click="breakUser()">Выйти</button>
                   </div>
                   <div class="flex items-center flex-no-shrink text-white ml-6">
                       <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
@@ -83,6 +84,10 @@ import List from './List'
         toggle() {
         this.open = !this.open
         this.menu_icon=!this.menu_icon
+        },
+          breakUser(){
+            this.$store.dispatch('rememberMe',{login:'',password:'',checked:false});
+            this.$router.push('/app');
         }
       }
     }
