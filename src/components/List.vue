bd038a5f5d8982e1b65b94b002b79215f5ad7044
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-21 10:39:20 +0300

    Update List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -33,38 +33,12 @@ computed :{
     lists(){
         return this.$store.state.list_category;
     }
-    ,
-    get_db(){
-        return this.$store.getters.get_db;
-    },
-    get_array(){
-       return this.$store.getters.get_array;
-    },
-    get_description(){
-        return this.$store.getters.get_description_;
-    },
-    get_obj(){
-        return this.$store.getters.get_obj
-    },
-    get_o(){
-       return this.$store.getters.get_cost_list_for_day;
-     },
-    costs_for_category(){
-        console.log(this.$store.state.cost_list);
-        return this.$store.state.cost_list;
-     },
-     history_cost_for_categorey(){
-         console.log(this.$store.state.history_cost_for_category)
-         return this.$store.state.history_cost_for_category;
-     }       
      , get_date_now(){
       return this.$store.getters.get_date_now;
     },
     get_time_now(){
       return this.$store.getters.get_time_now;
     },
-    // get_list_elements()
-
 },
 created(){
     this.$store.dispatch('select_',this.get_date_now);
