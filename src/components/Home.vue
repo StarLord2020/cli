04b73d4001d800e102ef798323fd2f5ca6f27c73
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-21 11:36:05 +0300

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
-    <div class="bg-red-500">
-      <div class="container  mx-auto">
+  <div class="bg-blue-500" id="app">
+    <div class="bg-red-500 fixed w-full z-10">
+      <div class="container mx-auto">
         <div class="content ">
-            <div class="font-sans antialiased fixed w-full" >
-              <div class="flex  ">
+            <div class="font-sans  antialiased  w-full" >
+              <div class="flex ">
                   <div class="block all:hidden">
                       <button @click="toggle" class=" red flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
                           <svg  class="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg> 
@@ -12,12 +12,19 @@
                   </div>
                   <div class="flex items-center flex-no-shrink text-white ml-6">
                       <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                      <span class="font-semibold text-xl tracking-tight">Keep Finance</span>
+                      <span class="font-semibold text-xl tracking-tight">Cost Accounting</span>
                   </div>    
-                  <nav class=" menu-list flex  bg-teal  bg-green-400 absolute top-20">
+                  <nav class=" menu-list flex w-full  bg-teal  bg-red-400 absolute top-20">
                       <div :class="open ? 'block': 'hidden'" class="w-full flex-grow all:flex p-6 all:items-center all:w-auto">
                           <div class="text-all all:flex-grow">
-                              <div class="link" @click="breakUser()">Выйти</div>
+                            <div class="link" >Пункт 1</div>
+                            <div class="link" >Пункт 2</div>
+                            <div class="link" >Пункт 3</div>
+                            <div class="link" >Пункт 4</div>
+                            <div class="link" >Пункт 5</div>
+                            <div class="link" >Пункт 6</div>
+                            <div class="link" >Пункт 7</div>
+                            <div class="link" @click="breakUser()">Выйти</div>
                           </div>             
                       </div>
                       <div>
@@ -31,21 +38,21 @@
           </div>
         </div>
       </div>
-      <div class="bg-gray-500">
+      <div class=" capital">
          <div class="container-mobile">
             <div class="container mx-auto">
               <capital></capital>
             </div>
           </div>
       </div>
-      <div class="bg-red-500">
+      <div class="">
         <div class="container-mobile">
           <div class="container mx-auto">
             <record><div class="mx-auto block w-10 text-white">Расходы</div></record>
           </div>
         </div>
       </div>
-      <div class="bg-blue-500">
+      <div class="">
         <div class="container-mobile">
           <div class="container mx-auto">
             <list></list>
@@ -91,6 +98,7 @@ import List from './List'
   }
   .menu-list{
       top: 37px;
+      max-width: 200px;
   }
   .close-icon{
   top: 0px;
@@ -111,5 +119,8 @@ import List from './List'
   }
   .link{
     cursor: pointer;
+  }
+  .capital{
+    padding-top: 38px;
   }
 </style>
