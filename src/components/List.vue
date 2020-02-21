17df03a456a44420e1f4adbacb33f7e15bb11ece
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-21 13:47:53 +0300

    Update List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
+    <div class="relative w-full">
         <ul>
-            <li ref="name_category"  @click="call_formhistory=true;getname(item.name)" class="flex w-3/6 mx-auto mb-8" v-for="item in lists" :key="item.id"><span class="flex"><img class="w-1/12" :src=item.img alt=""><span class="flex items-center">{{item.name}}</span></span><span class="flex items-center">{{item.cost}}$</span></li>
+            <li  @click="call_formhistory=true;getname(item.name)" class="flex w-3/6 mx-auto mb-8" v-for="item in lists" :key="item.id"><span class="flex"><img class="w-1/12" :src=item.img alt=""><span class="flex items-center">{{item.name}}</span></span><span class="flex items-center">{{item.cost}}$</span></li>
         </ul>
-        <div>
-            <button @click="call_waste=true">-</button>
+        <div class="absolute btn">
+            <button v-if='!call_waste' class="bg-red-500 hover:bg-red-700 text-white font-bold rounded-full w-6 h-6 relative" @click="call_waste=true"><span class="btn_span absolute">-</span></button>
+            <div class="absolute waste"><waste v-if="call_waste" @cancel="call_waste=false"></waste></div>
         </div>
-       <waste v-if="call_waste" @cancel="call_waste=false"></waste>
-        <form-history v-if="call_formhistory" :name="name" @cancel="call_formhistory=false"></form-history>
-  
+        <div>
+              <div class=" form-history absolute"><form-history v-if="call_formhistory" :name="name" @cancel="call_formhistory=false"></form-history></div>
+        </div>
+      
     </div> 
 </template>
 
@@ -43,7 +45,7 @@ computed :{
 }
 </script>
 
-<style>
+<style scoped>
 li:hover{
     opacity: 0.8;
      border-color: transparent;
@@ -52,5 +54,21 @@ li:hover{
 }
 li{
     cursor: pointer;
+}
+.btn_span{
+    top: -3px;
+    left: 9px;
+}
+.btn{
+top: 19px;
+right: 20px;
+}
+.waste{
+    right: -3px;
+
+}
+.form-history{
+    top: 6%;
+    left: 30%;;
 }
 </style>
